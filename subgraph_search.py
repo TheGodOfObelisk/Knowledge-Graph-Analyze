987d79139bb56ba6d7b29200f76f3decccb0769f
TheGodOfObelisk <1198582189@qq.com>
2019-11-29 20:30:54 +0800

    20191129

--- subgraph_search.py
+++ subgraph_search.py
@@ -29,6 +29,12 @@ import urllib
 # 从"2:0"点出发,一步可达的点集合,可以继续增加max_depth的值,当返回空时,max_depth=MAX_DISTANCE-1
 # 用不着最短路径了,自带的最短路径用不了
 
+# http://localhost:8080/graphs/hugegraph/traversers/rings?source=%222:0%22&max_depth=2&direction=OUT
+# 可以求从原点出发最大n步内的环路,hugegraph的Rings功能
+
+# http://localhost:8080/graphs/hugegraph/traversers/rays?source=%222:0%22&max_depth=2&direction=OUT
+# 根据起始顶点、方向、边的类型（可选）和最大深度等条件查找发散到边界顶点的路径,hugegraph的Rays功能
+
 # gremlin例子
 # g.V().as('a').out('icmp_echo_ping').as('b').select('a','b')
 # 由icmp_echo_ping类型的边相连的两个点的信息
@@ -88,6 +94,12 @@ import urllib
 # g.V('2:0').bothE().otherV().simplePath().path()
 # g.V('2:0').both().both().cyclicPath().path()
 
+# subGraph = g.E().or(__.hasLabel('icmp_echo_ping'),
+#                     __.hasLabel('icmp_echo_reply'))
+#                     .subgraph('subGraph').cap('subGraph').next()
+# sg = subGraph.traversal()
+# sg.E()
+# 多类型的边,生成的子图.即子图中既包含icmp_echo_ping类型的边,也包含icmp_echo_reply类型的边
 
 # 需要的gremline功能
 # 在图sg中,取出其中所有满足某个边属性条件的,边
@@ -133,31 +145,70 @@ def execute_command(cmd):
     str1 = sub.stdout.read()
     sub.communicate()
     str1 = str1.decode()
-    print str1
+    # print str1
     return str1
 
 def extract_max_distance(source_node_id):
+    print "获取攻击节点可达的最远距离..."
     max_distance = 1
-    url = "http://localhost:8080/graphs/hugegraph/traversers/kout"
-    pms = {
-        "source": "\"{source_node_id}\"",
-        "max_depth": max_distance
-    }
-    url_encoded = urllib.urlencode(pms)
-    # cmd = "curl " + url
-    # print cmd
-    print url_encoded
-    r = requests.get(url, params=pms)
-    print r.url
-    print r.status_code
+    while(True):
+        url = "http://localhost:8080/graphs/hugegraph/traversers/kout"
+        pms = {
+            "max_depth": max_distance
+        }
+        pms["source"] = "\"%s\""%source_node_id
+        url_encoded = urllib.urlencode(pms)
+        # cmd = "curl " + url
+        # print cmd
+        # print url_encoded
+        r = requests.get(url, params = pms)
+        # print r.url
+        # print r.status_code
+        # print type(r.content)
+        tmp_dict = json.loads(r.content)
+        end_while = False
+        for key in tmp_dict:
+            if key == "vertices" and tmp_dict[key] == []:
+                max_distance -= 1
+                end_while = True
+        if end_while:
+            break
+        max_distance += 1
     return max_distance
+
+def extract_key_events(pattern_num):
+    print "获取关键事件..."
+    tmp_events = set()
+    pattern = "attack_event_" + str(pattern_num)
+    with open("gremlin_scripts", "w") as f:
+        line = """g.V().outE('{attack_event_num}').values('event_label')\n""".format(attack_event_num = pattern)
+        f.write(line)
+        f.close()
+    cmd = hugegraph_bin_path + "hugegraph " + tool_command + " --file " + project_path + gremline_file_name
+    result = execute_command(cmd)
+    print result
+    lines = result.split('\n')
+    print lines
+    for item in lines:
+        if item != "" and item != "Run gremlin script":# Run gremlin script是hugegraph的输出
+            tmp_events.add(item)
+    return tmp_events
+
+def extract_event_chain_paths(MAX_DISTANCE):
+    print "提取攻击事件链(无环)..."
+    tmp_paths = []
+    # 攻击事件链结构
+    # 可以先用Rays方法求出其到边界节点的所有可能路径
+    # event(n1),event(n2),...,event(n3)
+    return tmp_paths
 
 if __name__ == '__main__':
     # cmd = hugegraph_bin_path + "hugegraph " + tool_command + " --file " + project_path + gremline_file_name
     # execute_command(cmd)
+    PATTERN_NUM = 0
     MAX_DISTANCE = 1 # ok
-    TIME_WINDOW = 0 # 自己设
-    KEY_EVENTS = [] # 取和0点相连的attack_event_n的边的event_label属性, ok
+    TIME_WINDOW = 600 # 自己设
+    KEY_EVENTS = set() # 取和0点相连的attack_event_n的边的event_label属性, ok
     EVENT_CHAIN_PATHS = []
     EVENT_CHAIN_CYCLICPATHS = []
     K = 1 # 自己设
@@ -166,11 +217,20 @@ if __name__ == '__main__':
     # K值(前K个可疑点),ok
     # 最大距离(限制匹配范围),ok
     # 环的处理(模式匹配/连续out匹配),环仍然要化为匹配规则
+    print "开始抽取攻击模式图0的特征信息:"
     source_node_id = "2:0" # 攻击特征图中的攻击节点
     MAX_DISTANCE = extract_max_distance(source_node_id)
     print "MAX_DISTANCE = " + str(MAX_DISTANCE)
+    print "设置时间窗大小..."
+    print "TIME_WINDOW = " + str(TIME_WINDOW)
+    print "设置K值..."
+    print "K = " + str(K)
     # 匹配规则的格式:
     # 可疑点出发,攻击事件链(思路,从攻击模式图中查找所有的事件链组合,重复也没事,链上每一步可能有不止一个事件(兼备,只要模式匹配能达到这个要求就行))
-
+    # 提取关键事件(用于求边生成子图)
+    KEY_EVENTS = extract_key_events(PATTERN_NUM)
+    print KEY_EVENTS
+    # 提取攻击事件链
+    EVENT_CHAIN_PATHS = extract_event_chain_paths(MAX_DISTANCE)
 
 
